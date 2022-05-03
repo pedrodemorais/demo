167c8001271c746e25bd123e6d8b691e739b1222
Pedro de Morais <pedrodemorais1@gmail.com>
2022-05-03 12:04:23 -0300

    JPA repository, injecao de dependencia, envio para banco de dados

---
+++ src/main/java/com/example/demo/config/TestConfig.java
@@ -0,0 +1,28 @@
+package com.example.demo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.example.demo.entidades.Usuario;
+import com.example.demo.repositories.UserRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Usuario u1 = new Usuario(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		Usuario u2 = new Usuario(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		userRepository.saveAll(Arrays.asList(u1,u2));
+	}
+	
+
+}
